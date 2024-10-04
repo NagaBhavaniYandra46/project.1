581787e37802205948ff4488c7fdfd623fcb6cee
NagaBhavaniYandra46 <yandranagabhavani@gmail.com>
2024-10-04 02:56:51 -0700

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,12 @@
+#pythone program to swap to numbers without using temporary variable
+a=4
+b=2
+print("Before swapping")
+print("a=",a)
+print("b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping")
+print("a=",a)
+print("b=",b)
